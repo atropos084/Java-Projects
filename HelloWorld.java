775922f2a345ad635f89c45ecef9840015d3d974
Don Keefauver <donkeefauver@student.olympic.edu>
2026-01-11 12:33:40 -0800

    Initial CS&141 Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main (String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+/* Don Keefauver
+   Hello World Assignment
+   CS&141
+   Instructor: Harmoney Peterson */
